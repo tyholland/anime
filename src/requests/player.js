1f057aaec7e55f2e107979bce70a67aa16f4cc30
tyholland <tyronehus@gmail.com>
2022-12-14 22:25:55 -0600

    refactor players

--- src/requests/player.js
+++ src/requests/player.js
@@ -2,7 +2,7 @@ import { axiosClient } from 'Utils/axios';
 const api = process.env.NEXT_PUBLIC_API;
 
 export const getPlayers = async () => {
-  const url = `${api}/character`;
+  const url = `${api}/player`;
   
   return await axiosClient(url);
 };
