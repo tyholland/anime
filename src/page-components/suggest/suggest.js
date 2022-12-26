4c6518c465deb8dc109432c3e8115e897c528b4e
tyholland <tyronehus@gmail.com>
2022-12-25 23:01:06 -0600

    add back link and remove unused file

--- src/page-components/suggest/suggest.js
+++ src/page-components/suggest/suggest.js
@@ -5,6 +5,7 @@ import { $GlobalContainer, $GlobalTitle } from 'Styles/global.style.js';
 import { $LoginWrapper } from 'PageComponents/login/login.style.js';
 import Select from 'Components/select';
 import { addEvent } from 'Utils/amplitude';
+import BackLink from 'Components/back-link';
 
 const Suggest = () => {
   const [player, setPlayer] = useState('');
@@ -29,31 +30,34 @@ const Suggest = () => {
   }, [player, series, rank]);
 
   return (
-    <$GlobalContainer>
-      <$LoginWrapper>
-        <$GlobalTitle>Suggest a Character</$GlobalTitle>
-        {!isSubmitted && (
-          <>
-            <TextField placeholder="Character Name" onChange={setPlayer} />
-            <TextField placeholder="Anime Series" onChange={setSeries} />
-            <Select
-              defaultVal="Character Rank"
-              onChange={setRank}
-              options={options}
-            />
-            <Button
-              btnText="Submit"
-              btnTextColor="black"
-              btnColor="orange"
-              customBtnClass="medium"
-              btnFunction={handleSubmit}
-              isDisabled={isDisabled}
-            />
-          </>
-        )}
-        {isSubmitted && <div>Thank you for your suggestion!</div>}
-      </$LoginWrapper>
-    </$GlobalContainer>
+    <>
+      <BackLink />
+      <$GlobalContainer>
+        <$LoginWrapper>
+          <$GlobalTitle>Suggest a Character</$GlobalTitle>
+          {!isSubmitted && (
+            <>
+              <TextField placeholder="Character Name" onChange={setPlayer} />
+              <TextField placeholder="Anime Series" onChange={setSeries} />
+              <Select
+                defaultVal="Character Rank"
+                onChange={setRank}
+                options={options}
+              />
+              <Button
+                btnText="Submit"
+                btnTextColor="black"
+                btnColor="orange"
+                customBtnClass="medium"
+                btnFunction={handleSubmit}
+                isDisabled={isDisabled}
+              />
+            </>
+          )}
+          {isSubmitted && <div>Thank you for your suggestion!</div>}
+        </$LoginWrapper>
+      </$GlobalContainer>
+    </>
   );
 };
 
