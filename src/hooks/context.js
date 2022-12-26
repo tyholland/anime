13db5869e628845e87212b6e3d4047187ba4e7d1
tyholland <tyronehus@gmail.com>
2022-12-25 21:14:35 -0600

    update context

--- src/hooks/context.js
+++ src/hooks/context.js
@@ -2,9 +2,45 @@ import { createContext, useContext, useState } from 'react';
 
 const AppContext = createContext();
 
+const formatUser = () => {
+  return {
+    userId: null,
+    email: null,
+    isActive: null,
+    firebaseId: null,
+    username: null,
+    totalPoints: null,
+  };
+};
+
+const formatLeague = () => {
+  return {
+    leagueId: null,
+    leagueName: null,
+    teamId: null,
+    teamName: null,
+    week: null,
+    players: {
+      captain: null,
+      brawlerA: null,
+      brawlerB: null,
+      bsBrawler: null,
+      bsSupport: null,
+      support: null,
+      villain: null,
+      battlefield: null,
+      benchA: null,
+      benchB: null,
+      benchC: null,
+      benchD: null,
+      benchE: null,
+    },
+  };
+};
+
 export const AppWrapper = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const [leagueDetails, setLeagueDetails] = useState(null);
+  const [currentUser, setCurrentUser] = useState(formatUser);
+  const [leagueDetails, setLeagueDetails] = useState(formatLeague);
 
   const sharedState = {
     currentUser,
