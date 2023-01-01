526483ff5d1f76c5494ad66d4cca16b44cb8d2fe
tyholland <tyronehus@gmail.com>
2023-01-01 14:36:43 -0600

    fix wording for gameplay and add filtering back to characters page

--- src/components/gameplay-card/boost.js
+++ src/components/gameplay-card/boost.js
@@ -7,23 +7,53 @@ const Boost = () => {
     <$GlobalContainer>
       <$GameplayCardSection>
         <$GameplayCardTitle>Weekly Element Affinity</$GameplayCardTitle>
-        <div>Characters with the same Affinity as the Weekly Element Affinity will recieve a boost. The character will recieve a percentage of the Weekly Element Affinity power boost. Characters that are affected by this boost are Captains and Brawlers.</div>
+        <div>
+          Characters with the same Affinity as the Weekly Element Affinity will
+          recieve a boost. The character will recieve a percentage of the Weekly
+          Element Affinity power boost.
+        </div>
       </$GameplayCardSection>
       <$GameplayCardSection>
         <$GameplayCardTitle>Support Character</$GameplayCardTitle>
-        <div>Support characters can boost allied characters. An allied character will recieve a percentage of the Support character's power boost. Characters will also have the chance to recieve a double boost if the Support character has two Affinities and the character has both of the same Affinities. Characters that are affected by this boost are Captains and Brawlers. However, Brawlers that are in the 2v2 Battle, are not affected by a normal Support Character.</div>
+        <div>
+          Support characters can boost allied characters. An allied character
+          will recieve a percentage of the Support character's power boost.
+          Characters will also have the chance to recieve a double boost if the
+          Support character has two Affinities and the character has both of the
+          same Affinities. Characters that are affected by this boost are
+          Captains and Brawlers. However, Brawlers that are in the 2v2 Battle,
+          are not affected by a normal Support Character.
+        </div>
       </$GameplayCardSection>
       <$GameplayCardSection>
         <$GameplayCardTitle>Support Character in 2v2 Battle</$GameplayCardTitle>
-        <div>Support characters in a 2v2 Battle can only boost the Brawler they are teamed up with. The Brawler will recieve a percentage of that Support character's power boost. The Brawler will also have the chance to recieve a double boost if the Support character has two Affinities and the Brawler has both of the same Affinities. Captains and other Brawlers, not in the 2v2 Battle, will not be affected by the Support character in a 2v2 Battle.</div>
+        <div>
+          Support characters in a 2v2 Battle can only boost the Brawler they are
+          teamed up with. The Brawler will recieve a percentage of that Support
+          character's power boost. The Brawler will also have the chance to
+          recieve a double boost if the Support character has two Affinities and
+          the Brawler has both of the same Affinities. Captains and other
+          Brawlers, not in the 2v2 Battle, will not be affected by the Support
+          character in a 2v2 Battle.
+        </div>
       </$GameplayCardSection>
       <$GameplayCardSection>
         <$GameplayCardTitle>Battlefield</$GameplayCardTitle>
-        <div>Characters with the same Affinity as the Battlefield will recieve a boost. A Battlefield can also boost an allied character without an Affinity. The character will recieve a percentage of the Battlefield power boost. Characters will also have the chance to recieve a double boost if the Battlefield has two Affinities and the character has both of the same Affinities. Characters that can be affected by this boost are Captains, Brawlers, Support, and Villains.</div>
+        <div>
+          Characters with the same Affinity as the Battlefield will recieve a
+          boost. A Battlefield can also boost an allied character without an
+          Affinity. The character will recieve a percentage of the Battlefield
+          power boost. Characters will also have the chance to recieve a double
+          boost if the Battlefield has two Affinities and the character has both
+          of the same Affinities.
+        </div>
       </$GameplayCardSection>
       <$GameplayCardSection>
         <$GameplayCardTitle>User Voting</$GameplayCardTitle>
-        <div>Characters in a specific Head-to-Head Battle can recieve a boost. The character will recieve the full amount of the User Voting power boost. Characters that can be affected by this boost are Captains, Brawlers, Support, and Villains.</div>
+        <div>
+          Characters in a specific Head-to-Head Battle can recieve a boost. The
+          character will recieve the full amount of the User Voting power boost.
+        </div>
       </$GameplayCardSection>
     </$GlobalContainer>
   );
--- src/components/gameplay-card/loss.js
+++ src/components/gameplay-card/loss.js
@@ -7,19 +7,34 @@ const Loss = () => {
     <$GlobalContainer>
       <$GameplayCardSection>
         <$GameplayCardTitle>Weekly Element Affinity</$GameplayCardTitle>
-        <div>Characters with the same weakness as the Weekly Element Affinity will recieve damage. The character will lose a percentage of the Weekly Element Affinity power boost. Characters that will receieve damage are Captains and Brawlers.</div>
+        <div>
+          Characters with the same weakness as the Weekly Element Affinity will
+          recieve damage. The character will lose a percentage of the Weekly
+          Element Affinity power boost.
+        </div>
       </$GameplayCardSection>
       <$GameplayCardSection>
         <$GameplayCardTitle>Villains</$GameplayCardTitle>
-        <div>Characters with the same weakness as the Villain's Affinity will lose points. The character will lose a percentage of the Villain's damage. Characters that will receieve damage are Captains and Brawlers.</div>
+        <div>
+          Characters with the same weakness as the Villain's Affinity will lose
+          points. The character will lose a percentage of the Villain's damage.
+        </div>
       </$GameplayCardSection>
       <$GameplayCardSection>
         <$GameplayCardTitle>Battlefield</$GameplayCardTitle>
-        <div>Characters with the same weakness as the Battlefield will recieve damage. A Battlefield can also damage allied characters without an Affinity. The character will lose a percentage of the Battlefield power boost. Characters that can be affected by this boost are Captains, Brawlers, Support, and Villains.</div>
+        <div>
+          Characters with the same weakness as the Battlefield will recieve
+          damage. A Battlefield can also damage allied characters without an
+          Affinity. The character will lose a percentage of the Battlefield
+          power boost.
+        </div>
       </$GameplayCardSection>
       <$GameplayCardSection>
         <$GameplayCardTitle>User Voting</$GameplayCardTitle>
-        <div>Characters in a specific Head-to-Head Battle can recieve damage. The character will lose the full amount of the User Voting power loss. Characters that can be affected by this boost are Captains, Brawlers, Support, and Villains.</div>
+        <div>
+          Characters in a specific Head-to-Head Battle can recieve damage. The
+          character will lose the full amount of the User Voting power loss.
+        </div>
       </$GameplayCardSection>
     </$GlobalContainer>
   );
