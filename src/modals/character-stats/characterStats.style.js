937566b7ee9be9be0ca6ecdce795ca133fb06b08
tyholland <tyronehus@gmail.com>
2023-01-06 08:26:54 -0600

    add new files

---
+++ src/modals/character-stats/characterStats.style.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+export const $CharacterStatsBtnWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  button {
+    margin: 0 2%;
+  }
+`;
+
+export const $CharacterStatsScoring = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 20px;
+
+  &.total {
+    border-top: 1px solid #000;
+    padding-top: 20px;
+  }
+`;
+
+export const $CharacterStatsPoints = styled.div`
+  text-align: left;
+  width: 48%;
+  padding-left: 2%;
+  font-size: 20px;
+
+  &.specific {
+    font-size: 14px;
+    color: rgba(0, 0, 0, 0.5);
+  }
+`;
+
+export const $CharacterStatsLabel = styled.div`
+  text-align: right;
+  width: 48%;
+  padding-right: 2%;
+  font-weight: 700;
+  font-size: 20px;
+
+  &.specific {
+    font-size: 14px;
+    color: rgba(0, 0, 0, 0.5);
+  }
+`;
---
+++ src/modals/character-stats/index.js
@@ -0,0 +1,146 @@
+import React from 'react';
+import Button from '../../components/button';
+import { $GlobalTitle } from 'Styles/global.style';
+import {
+  $CharacterStatsBtnWrapper,
+  $CharacterStatsScoring,
+  $CharacterStatsPoints,
+  $CharacterStatsLabel,
+} from './characterStats.style';
+import MainModal from '../main';
+
+const CharacterStats = ({ isModalOpen, setIsModalOpen, character }) => {
+  const shareEvent = async () => {
+    alert('This is a test');
+  };
+
+  const customStyles = {
+    content: {
+      position: 'relative',
+      maxWidth: '500px',
+      maxHeight: '600px',
+    },
+    overlay: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  if (!character) {
+    return;
+  }
+
+  const { name, matchPoints, damage, boost, originalPower, teamPoints } =
+    character;
+  const boostTotal = teamPoints - originalPower;
+  const damageTotal = teamPoints - matchPoints;
+
+  return (
+    <MainModal
+      modalIsOpen={isModalOpen}
+      closeModal={closeModal}
+      styles={customStyles}
+    >
+      <$GlobalTitle>{name}</$GlobalTitle>
+      <$CharacterStatsScoring>
+        <$CharacterStatsLabel>Points</$CharacterStatsLabel>
+        <$CharacterStatsPoints>{originalPower}</$CharacterStatsPoints>
+      </$CharacterStatsScoring>
+      <$CharacterStatsScoring>
+        <$CharacterStatsLabel>Boost</$CharacterStatsLabel>
+        <$CharacterStatsPoints>+ {boostTotal}</$CharacterStatsPoints>
+      </$CharacterStatsScoring>
+      <$CharacterStatsScoring>
+        <$CharacterStatsLabel className="specific">
+          Weekly Affinity
+        </$CharacterStatsLabel>
+        <$CharacterStatsPoints className="specific">
+          {boost.week}
+        </$CharacterStatsPoints>
+      </$CharacterStatsScoring>
+      <$CharacterStatsScoring>
+        <$CharacterStatsLabel className="specific">
+          Support
+        </$CharacterStatsLabel>
+        <$CharacterStatsPoints className="specific">
+          {boost.support}
+        </$CharacterStatsPoints>
+      </$CharacterStatsScoring>
+      <$CharacterStatsScoring>
+        <$CharacterStatsLabel className="specific">
+          Battlefield
+        </$CharacterStatsLabel>
+        <$CharacterStatsPoints className="specific">
+          {boost.battlefield}
+        </$CharacterStatsPoints>
+      </$CharacterStatsScoring>
+      <$CharacterStatsScoring>
+        <$CharacterStatsLabel className="specific">Voting</$CharacterStatsLabel>
+        <$CharacterStatsPoints className="specific">
+          {boost.voting}
+        </$CharacterStatsPoints>
+      </$CharacterStatsScoring>
+      <$CharacterStatsScoring>
+        <$CharacterStatsLabel>Damage</$CharacterStatsLabel>
+        <$CharacterStatsPoints>- {damageTotal}</$CharacterStatsPoints>
+      </$CharacterStatsScoring>
+      <$CharacterStatsScoring>
+        <$CharacterStatsLabel className="specific">
+          Weekly Affinity
+        </$CharacterStatsLabel>
+        <$CharacterStatsPoints className="specific">
+          {damage.week}
+        </$CharacterStatsPoints>
+      </$CharacterStatsScoring>
+      <$CharacterStatsScoring>
+        <$CharacterStatsLabel className="specific">
+          Villain
+        </$CharacterStatsLabel>
+        <$CharacterStatsPoints className="specific">
+          {damage.villain}
+        </$CharacterStatsPoints>
+      </$CharacterStatsScoring>
+      <$CharacterStatsScoring>
+        <$CharacterStatsLabel className="specific">
+          Battlefield
+        </$CharacterStatsLabel>
+        <$CharacterStatsPoints className="specific">
+          {damage.battlefield}
+        </$CharacterStatsPoints>
+      </$CharacterStatsScoring>
+      <$CharacterStatsScoring>
+        <$CharacterStatsLabel className="specific">Voting</$CharacterStatsLabel>
+        <$CharacterStatsPoints className="specific">
+          {damage.voting}
+        </$CharacterStatsPoints>
+      </$CharacterStatsScoring>
+      <$CharacterStatsScoring className="total">
+        <$CharacterStatsLabel>Total Points</$CharacterStatsLabel>
+        <$CharacterStatsPoints>{matchPoints}</$CharacterStatsPoints>
+      </$CharacterStatsScoring>
+      <$CharacterStatsBtnWrapper>
+        <Button
+          btnText="Get Votes"
+          btnTextColor="black"
+          btnColor="orange"
+          customBtnClass="medium"
+          btnFunction={shareEvent}
+        />
+        <Button
+          btnText="Close"
+          btnTextColor="white"
+          btnColor="red"
+          customBtnClass="medium"
+          btnFunction={() => setIsModalOpen(!isModalOpen)}
+        />
+      </$CharacterStatsBtnWrapper>
+    </MainModal>
+  );
+};
+
+export default CharacterStats;
