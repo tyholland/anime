53d537a6960ce1238f5c1b60d4a5a7421ef90db5
tyholland <tyronehus@gmail.com>
2023-02-18 22:55:32 -0600

    refactor firebase functionality

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,14 +1,24 @@
 import { initializeApp } from 'firebase/app';
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_GOOGLE_API,
-  authDomain: `${process.env.NEXT_PUBLIC_GOOGLE_PRODJECT_ID}.firebaseapp.com`,
-  projectId: process.env.NEXT_PUBLIC_GOOGLE_PRODJECT_ID,
-  storageBucket: `${process.env.NEXT_PUBLIC_GOOGLE_PRODJECT_ID}.appspot.com`,
-  messagingSenderId: process.env.NEXT_PUBLIC_GOOGLE_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_GOOGLE_APP_ID,
+let firebaseConfig;
+
+const setupFirebase = () => {
+  firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_GOOGLE_API,
+    authDomain: `${process.env.NEXT_PUBLIC_GOOGLE_PRODJECT_ID}.firebaseapp.com`,
+    projectId: process.env.NEXT_PUBLIC_GOOGLE_PRODJECT_ID,
+    storageBucket: `${process.env.NEXT_PUBLIC_GOOGLE_PRODJECT_ID}.appspot.com`,
+    messagingSenderId: process.env.NEXT_PUBLIC_GOOGLE_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_GOOGLE_APP_ID,
+  };
+
+  initializeApp(firebaseConfig);
 };
 
 export const firebaseApp = () => {
-  initializeApp(firebaseConfig);
+  if (firebaseConfig) {
+    return initializeApp(firebaseConfig);
+  }
+
+  return setupFirebase();
 };
