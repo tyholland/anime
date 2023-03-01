a8cb38d7b97c500afc4f2f9a869e3be5a6c34b0b
tyholland <tyronehus@gmail.com>
2023-02-28 22:13:09 -0600

    add mobile styling to fun fact

--- src/components/read-more/readMore.style.js
+++ src/components/read-more/readMore.style.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { COLOR_GREY } from 'Styles/global.style';
+import { COLOR_GREY, MOBILE_VIEW } from 'Styles/global.style';
 
 export const $ReadMoreWrapper = styled.div`
   position: absolute;
@@ -12,6 +12,11 @@ export const $ReadMoreContent = styled.div`
   padding: 1%;
   width: 50%;
   border-radius: 10px;
+
+  ${MOBILE_VIEW} {
+    padding: 3%;
+    width: 90%;
+  }
 
   &.show {
     display: block;
