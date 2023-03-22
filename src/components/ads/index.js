b44ef8dbddb6955a8f6bc99ef8afe950bf5f6804
tyholland <tyronehus@gmail.com>
2023-03-21 23:31:32 -0500

    add ads files

---
+++ src/components/ads/index.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+// import AdSense from 'react-adsense';
+
+const Ads = () => {
+  useEffect(() => {
+    var ads = document.getElementsByClassName('adsbygoogle').length;
+
+    for (var i = 0; i < ads; i++) {
+      try {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }, []);
+
+  return (
+    <div>
+      <ins
+        className="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-6648399860832112"
+        data-ad-slot="4473046527"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+      {/* <AdSense.Google
+        client="ca-pub-6648399860832112"
+        slot="4473046527"
+        style={{ display: "block" }}
+        format="auto"
+        responsive="true"
+      /> */}
+    </div>
+  );
+};
+
+export default Ads;
