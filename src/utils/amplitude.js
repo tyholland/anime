a64d9bb68a6f15fd556dc8f55ef7a26c86260a3c
tyholland <tyronehus@gmail.com>
2023-05-07 23:45:01 -0500

    do not send error message for status 400

--- src/utils/amplitude.js
+++ src/utils/amplitude.js
@@ -3,6 +3,10 @@ import { init, track } from '@amplitude/analytics-browser';
 init(process.env.NEXT_PUBLIC_AMPLITUDE_KEY);
 
 export const addEvent = (event, properties) => {
+  if (properties.status === 400) {
+    return;
+  }
+
   if (!properties) {
     track(event);
   }
