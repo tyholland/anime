22044296e98499f147d2f325bbe354b8354208f5
tyholland <tyronehus@gmail.com>
2023-05-13 20:54:43 -0500

    left align series in mobile

--- src/styles/global.style.js
+++ src/styles/global.style.js
@@ -286,6 +286,7 @@ export const $GlobalContainer = styled.div`
         text-overflow: ellipsis;
         overflow: hidden;
         white-space: nowrap;
+        text-align: left;
       }
     }
   }
